cbecbe79266853df7d75eca49687bdb57bf9d2f6
ANK9823 <85231019+ANK9823@users.noreply.github.com>
2021-07-04 17:38:03 +0530

    Delete python and netwoking C1.py

--- python and netwoking C1.py
+++
@@ -1,7 +0,0 @@
-import hashlib
-
-txt = "This is my first challenge under Python and Network security bootcamp"
-hash_obt = hashlib.md5(txt.encode())
-md5_hash = hash_obt.hexdigest()
-
-print(md5_hash)
